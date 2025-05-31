8500a02ff2d502e6b4a3e733fea7def58c7a098c
I-Am-Shimanto <shimantowebdeveloper@gmail.com>
2025-05-31 13:06:15 +0600

    complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { app } from './dbconfig.ts'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { app } from "./dbconfig.ts";
 
-createRoot(document.getElementById('root')!).render(
-
-    <App />
-
-)
+createRoot(document.getElementById("root")!).render(<App />);
